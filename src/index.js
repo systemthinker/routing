acb89c5bac1a6151efa7e556bcef9874ebd3be81
systemthinker <robbertoutenaar@gmail.com>
2020-05-01 14:46:31 +0200

    installed react-router-dom

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import { BrowserRouter as Router } from "react-router-dom";
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
@@ -5,9 +6,9 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Router>
     <App />
-  </React.StrictMode>,
+  </Router>,
   document.getElementById('root')
 );
 
