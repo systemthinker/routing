06d7e69891ffc7d3c57bdc1d72ceb5fc937f5eb3
systemthinker <robbertoutenaar@gmail.com>
2020-05-02 13:01:32 +0200

    store data from movie api, return ul with images as a list

--- src/pages/DiscoverMoviesPage.js
+++ src/pages/DiscoverMoviesPage.js
@@ -1,5 +1,64 @@
-import React from 'react';
+import React, { useState } from "react";
+import axios from 'axios';
 
-export default function DiscoverMoviesPage(){
-    return <p>Discover Movies</p>
+export default function DiscoverMoviesPage() {
+  const [searchText, set_searchText] = useState("");
+
+  const [searching, set_searching] = useState("idle...");
+
+  const [data, set_data] = useState([]);
+
+ 
+
+ 
+
+  const search = async () => {
+    set_searching("starting search");
+    console.log("Start searching for:", searchText);
+  
+    // Best practice: encode the string so that special characters
+    //  like '&' and '?' don't accidentally mess up the URL
+    const queryParam = encodeURIComponent(searchText);
+  
+  
+ 
+  
+    
+    let dataFromApi = await axios.get(
+      `https://omdbapi.com/?apikey=f5f45c92&s=${queryParam}`
+    );
+    
+
+    set_data(dataFromApi.data.Search);
+    console.log('set data complete',data)
+  
+    set_searching(`search for ${searchText} complete!`);
+  };
+
+  console.log(data);
+
+  return (
+    <div>
+      <h1>Discover some movies!</h1>
+      <p>
+        <input
+          value={searchText}
+          onChange={e => set_searchText(e.target.value)}
+          
+        
+        />
+        <button onClick={search}>Search</button>
+      </p>
+      <p>
+         {searching}
+      </p>
+
+      <ul>
+          {data.map((poster)=> {
+              return (<img src={poster.Poster}></img>)
+          })}
+          
+      </ul>
+    </div>
+  );
 }
